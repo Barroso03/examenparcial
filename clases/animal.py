d301283398a5c4d2e364435775ed9b599904863b
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-03-24 08:36:52 +0000

    terminamos clases libro y animal

---
+++ clases/animal.py
@@ -0,0 +1,14 @@
+class animal():
+    def __init__(self,oviparo,mamifero,):
+        self.oviparo = oviparo
+        self.mamifero = mamifero
+    def oviparo():
+        print("Animales oviparos:")
+        print("pollo")
+        print("ornitorrinco")
+    def mamifero():
+        print("Animales mamiferos:")
+        print("gato")
+print("Ejercicio 2:")   
+print(animal.oviparo())
+print(animal.mamifero())
