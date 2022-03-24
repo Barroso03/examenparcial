8d47a78da35eef298ce4d602c0cbfebbe9bb9d59
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-03-24 08:01:27 +0000

    creamos la clase libro con el constructor y las funciones

---
+++ clases/libro.py
@@ -0,0 +1,19 @@
+class libro():
+   def __init__(self,autor,ISBN,titulo):
+    self.autor = autor
+    self.ISBN = ISBN
+    self.titulo = titulo
+   def autor():
+       print("Nombre del autor:")
+       print("Jorge Barroso")
+   def ISBN():
+       print("ISBN:")
+       print("123456789")
+   def titulo():
+       print("Titulo del libro:")
+       print("Los bro sabemos")
+
+print("Ejercicio 1:")
+print(libro.autor())
+print(libro.ISBN())
+print(libro.titulo())
